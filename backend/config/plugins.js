afc8926683cd409bafa1caf677947db795739213
mikebcbc <mikebconstantino@gmail.com>
2022-12-08 11:16:20 -0500

    add spaces compatability

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -11,4 +11,22 @@ module.exports = ({ env }) => ({
   "export-import-form": {
     enabled: true,
   },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
