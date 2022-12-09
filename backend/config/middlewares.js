e4bd182f43b1b7c5f5aa17e23a60dc5cf8160143
mikebcbc <mikebconstantino@gmail.com>
2022-12-08 21:58:52 -0500

    allow http

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
+          "connect-src": ["'self'", "https:", "http:"],
           "img-src": [
             "'self'",
             "data:",
