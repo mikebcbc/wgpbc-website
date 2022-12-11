569839dc79e10a2fd20f17c84a077a4d027c26a9
mikebcbc <mikebconstantino@gmail.com>
2022-12-10 20:53:32 -0500

    remove redudany plugins and disable drafts on sermons

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,14 +1,5 @@
 module.exports = ({ env }) => ({
-  "generate-data": {
-    enabled: true,
-  },
-  "duplicate-button": {
-    enabled: true,
-  },
   "import-export-entries": {
-    enabled: true,
-  },
-  "export-import-form": {
     enabled: true,
   },
   upload: {
