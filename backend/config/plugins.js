72ab96fae1492e17a03629daf72202596ed6514a
mikebcbc <mikebconstantino@gmail.com>
2023-12-10 15:55:15 -0500

    add aws s3 region

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -6,11 +6,16 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-        endpoint: env("DO_SPACE_ENDPOINT"),
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+            secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+          },
+          endpoint: env("DO_SPACE_ENDPOINT"), // aws-s3 v3 needs a prefixed https:// endpoint
+          region: env("DO_SPACE_REGION"),
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+          },
         },
       },
       actionOptions: {
