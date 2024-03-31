29454608a53f30f6df14e2a20bd8f9b09e17ea76
mikebcbc <mikebconstantino@gmail.com>
2024-03-31 18:31:24 -0400

    contiunued revert

--- frontend/src/templates/meeting/style.js
+++
@@ -1,8 +0,0 @@
-import styled, { themeGet, device } from "@theme/utils";
-
-export const SermonsContainer = styled.section`
-    padding: ${themeGet("padding.rowPadding.md")};
-    ${device.large} {
-        padding: ${themeGet("padding.rowPadding.lg")};
-    }
-`;
